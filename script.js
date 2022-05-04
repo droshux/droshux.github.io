52c88a0ed4fd619af58056e597246f4d30f5a7b7
Huxley Leigh <16122HLEI@cotham.bristol.sch.uk>
2022-05-04 15:58:48 +0100

    Trying to get the animations to work :)

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ for (i=0; i<dropdownArrows.length;i++) {
     dropdownArrows[i].setAttribute("data-rotationvalue", String(0));
     rval = parseInt(dropdownArrows[i].getAttribute("data-rotationvalue"))
     dropdownArrows[i].addEventListener("animationend", function (event) {
-        event.target.style.animation = "idle"
+        //event.target.style.animation = "idle"
         if (event.target.getAttribute("data-rotated") == "0") {
             event.target.setAttribute("data-rotated", "1")
+        } else {
+            event.target.setAttribute("data-rotated", "0")
         }
     })
     dropdownArrows[i].addEventListener("click", function (event) {
         if (event.target.getAttribute("data-rotated") == "0") {
-            event.target.style.animation = "spinUP 0.5s"
+            event.target.style.animation = "spinUP 0.5s forwards"
+        } else {
+            event.target.style.animation = "spinUP 0.5s forwards"
         }
     })
 }
