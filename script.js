7ed0ce4805e93f96243cbc86c9d31cfbb79ed217
Huxley Leigh <16122HLEI@cotham.bristol.sch.uk>
2022-05-04 16:59:10 +0100

    Revert "BUG FIXED! Long live event.currentTarget!"
    This reverts commit d4d113be4217df4263219a090f9a2fc1d417d602.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ for (i=0; i<svgs.length; i++) {
 //dropdownArrows.push(document.getElementById("body"))
 for (i = 0; i < dropdownArrows.length; i++) {
     dropdownArrows[i].addEventListener("animationend", function (event) {
+        //event.target.style.animation = "idle"
         if (event.target.getAttribute("data-rotated") == "0") {
             event.target.setAttribute("data-rotated", "1")
         } else {
@@ -15,10 +16,12 @@ for (i = 0; i < dropdownArrows.length; i++) {
         }
     })
     dropdownArrows[i].addEventListener("click", function (event) {
-        if (event.currentTarget.getAttribute("data-rotated") == "0") {
-            event.currentTarget.style.animation = "spinUP 0.5s forwards"
-        } else {
-            event.currentTarget.style.animation = "spinDOWN 0.5s forwards"
+        if (event.target.tagName != "use") {
+            if (event.target.getAttribute("data-rotated") == "0") {
+                event.target.style.animation = "spinUP 0.5s forwards"
+            } else {
+                event.target.style.animation = "spinDOWN 0.5s forwards"
+            }
         }
     })
 }
