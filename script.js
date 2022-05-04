a718688009cd54779f7c223e4be3b29473a857d1
Huxley Leigh <16122HLEI@cotham.bristol.sch.uk>
2022-05-04 15:19:45 +0100

    We're gonna do animations instead now

--- script.js
+++ script.js
@@ -10,32 +10,10 @@ for (i=0; i<dropdownArrows.length;i++) {
     dropdownArrows[i].setAttribute("data-rotationvalue", String(0));
     rval = parseInt(dropdownArrows[i].getAttribute("data-rotationvalue"))
     dropdownArrows[i].addEventListener("click", function(event) {
-        if (rval%360 < 240) {
-            t = Math.floor(rval/360)*360 + 240
-        } else {
-            t = Math.floor(rval/360)*360 + 420
-        }
-        spinterval = setInterval(() => rotateItem(event.target, t), 75)
+        
     })
 }
 
-const rotationSpeed = 60;
 function rotateItem(item, target) {
-    rval = parseInt(item.getAttribute("data-rotationvalue"))
-    if (rval < target) {
-        item.style.transform = "rotate(" + rval + "deg)";
-        item.style.webkitTransform = "rotate(" + rval + "deg)";
-        item.style.mozTransform = "rotate(" + rval + "deg)";
-        rval += rotationSpeed;
-        item.setAttribute("data-rotationvalue", String(rval)); 
-    } else {
-        imagesID = item.getAttribute("data-imagesid")
-        images = document.getElementById(imagesID)
-        if (rval%360 == 240) {
-            images.style = "display: block;"
-        } else {
-            images.style = "display: none;"
-        }
-        clearInterval(spinterval)
-    }
+    
 }
