ef34f2f411e9ef59f81380a396675a91dd5597fd
Huxley Leigh <16122HLEI@cotham.bristol.sch.uk>
2022-05-04 17:02:33 +0100

    Actually commiting silly

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ for (i = 0; i < dropdownArrows.length; i++) {
         }
     })
     dropdownArrows[i].addEventListener("click", function (event) {
-        if (event.target.tagName != "use") {
-            if (event.target.getAttribute("data-rotated") == "0") {
-                event.target.style.animation = "spinUP 0.5s forwards"
+        if (event.currentTarget.tagName != "use") {
+            if (event.currentTarget.getAttribute("data-rotated") == "0") {
+                event.currentTarget.style.animation = "spinUP 0.5s forwards"
             } else {
-                event.target.style.animation = "spinDOWN 0.5s forwards"
+                event.currentTarget.style.animation = "spinDOWN 0.5s forwards"
             }
         }
     })
